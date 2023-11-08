995e987cad624c57571884a79666ccd6cbb144fb
Sungwoo Cho <seouk77@gmail.com>
2023-11-09 01:30:20 +0900

    231109 2798 v2

--- boj/2798_sungwoo.py
+++ boj/2798_sungwoo.py
@@ -1,15 +1,15 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
-combs = combinations(cards, 3)  # cards 중 원소 3개의 조합 생성
 result = 0
-for comb in combs:  # 조합을 순회하며
-    sumOfComb = sum(comb)  # 각 조합의 합이
-    if sumOfComb <= m and sumOfComb > result:  # m보다 작거나 같고, result보다 크다면 갱신
-        result = sumOfComb
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            sumOfComb = cards[i] + cards[j] + cards[k]
+            if sumOfComb <= m and sumOfComb > result:
+                result = sumOfComb
 
 print(result)
