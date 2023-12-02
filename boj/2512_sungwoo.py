5c9b2275ad3c5f25a87a49fb4c22d033edff435a
Sungwoo Cho <seouk77@gmail.com>
2023-12-03 00:46:03 +0900

    231203 2512 (이분탐색)

--- boj/2512_sungwoo.py
+++ boj/2512_sungwoo.py
@@ -2,20 +2,26 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-requests = sorted(list(map(int, input().split())), reverse=True)
+requests = sorted(list(map(int, input().split())))
 total = int(input())
 
-result = requests[0]  # max request, 상한액
 
-while result >= 0:  # 상한액을 줄여가며 조건에 맞는 상한액을 탐색
+start, end = 0, requests[-1]
+prev = 0
 
-    tmpTotal, i = total, 0
-    while i < n:  # 정렬된 리스트를 순회하며
-        tmpTotal -= (requests[i] if requests[i] <= result else result)  # 임시 총액에서 상한액에 따른 예산을 차감
+while start <= end:
+    mid = (start + end) // 2
+    limit = mid
+
+    tmpTotal, i = total, n - 1
+    while i >= 0:  # 정렬된 리스트를 순회하며
+        tmpTotal -= (requests[i] if requests[i] <= limit else limit)  # 임시 총액에서 상한액에 따른 예산을 차감
         if tmpTotal < 0:  # 만약 임시 총액이 음수가 된다면 상한액을 더 줄여야 하는 것이므로 break
+            end = mid - 1
             break
-        i += 1
-    else:  # braek에 걸리지 않았다면 조건을 만족하는 상한액이므로 출력 후 break
-        print(result)
-        break
-    result -= 1
+        i -= 1
+    else:  # braek에 걸리지 않았다면 조건을 만족하는 상한액이나 최댓값을 찾아야하기 때문에 더 높은 상한액 탐색을 위해 범위를 위쪽으로 좁힘
+        prev = limit  # 해당 값이 최댓값이었을 수 있기 때문에 prev에 저정함
+        start = mid + 1
+
+print(prev)  # 사전에 저장해둔 최댓값 출력
