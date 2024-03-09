84c091ed32494cb728c128feee19adb21c860b44
m0onsoo <moonsoo2008@gmail.com>
2024-03-09 22:16:04 +0900

    240309

--- boj/Dynamic Programming/1010_moonsoo.py
+++ boj/Dynamic Programming/1010_moonsoo.py
@@ -3,13 +3,31 @@ input= sys.stdin.readline
 
 T = int(input())
 
-dp = [[0 for j in range(31)] for i in range(31)]
+# m개의 오른쪽 다리 중 연결 가능한 왼쪽의 n개인 mCn을 구하는 문제
+# DP적으로 접근하면 mCn = m-1Cn + m-1Cn-1
+def mCn(m, n):
+    if dp[m][n] > 0:
+        # 값이 이미 있으면 가져다 사용
+        return dp[m][n]
+    elif dp[m][n] < 0:
+        return 0
+    
+    elif dp[m][n] == 0:
+        # 아직 값이 존재하지 않으면 앞에 것들 조회
+        return mCn(m-1, n) + mCn(m-1, n-1)
 
 
-# a[3][4] = a[2][3] + a[3][3]
-# dp[n] = n-1Cm-1 + nCm-1 = 
-
+dp = [[0 for j in range(30)] for i in range(30)]
+for i in range(1, 30):
+    for j in range(1, 30):
+        if i < j:
+            dp[i][j] = -1
+        
+        if j == 1:
+            dp[i][1] = i
+        # 초기화
+            
 for _ in range(T):
     n, m = map(int, input().split())
 
-    dp[n][m] = dp[n-1][m-1] + dp[n][m-1]
+    print(mCn(m, n))
