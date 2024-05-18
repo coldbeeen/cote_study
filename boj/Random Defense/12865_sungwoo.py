fea16650bcd8018218981dbbaf86f43968cabfc5
Sungwoo Cho <seouk77@gmail.com>
2024-05-18 16:30:10 +0900

    240517 12865 (updated)

--- boj/Random Defense/12865_sungwoo.py
+++ boj/Random Defense/12865_sungwoo.py
@@ -11,7 +11,8 @@ for m in range(1, k+1):  # 배낭 최대 허용 무게 (max_weight)
     for c in range(1, n+1):  # c번째 아이템까지 고려하였을 때 (count)
         w, v = items[c][0], items[c][1]
 
-        if w <= m:  # 해당 아이템이 무게가 최대 허용 무게를 넘기지 않는다면
-            dp[m][c] = max(dp[m][c-1], v + dp[m-w][c-1])  # DP 값 업데이트
+        # 해당 아이템이 무게가 최대 허용 무게를 넘기지 않는다면
+        # 해당 아이템의 무게를 뺀 c-1의 DP 값과 v를 더하여 이 값이 더 크다면 업데이트
+        dp[m][c] = max(dp[m][c-1], v + dp[m-w][c-1] if w <= m else 0)
 
 print(dp[k][n])
