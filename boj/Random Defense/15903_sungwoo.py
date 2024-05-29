a089b9548655648b615483fe70ea0bc9ab841ce6
Sungwoo Cho <seouk77@gmail.com>
2024-05-29 16:35:52 +0900

    240529 15903 (우선순위 큐 버전)

--- boj/Random Defense/15903_sungwoo.py
+++ boj/Random Defense/15903_sungwoo.py
@@ -1,12 +1,11 @@
+from heapq import *
+
 n, m = map(int, input().split())
 card_list = list(map(int, input().split()))
+heapify(card_list)  # 우선순위 큐 (힙 큐) 변환
 
 for i in range(m):
-    m1 = min(card_list)  # 첫번째 최솟값
-    card_list.remove(m1)  # 첫번째 최솟값 제거
-    m2 = min(card_list)  # 두번째 최솟값
-    card_list.remove(m2)  # 두번째 최솟값 제거
-    sum_of_two_cards = m1 + m2  # 최솟값 더하여 합친 후
-    card_list.extend([sum_of_two_cards, sum_of_two_cards])  # 합친 카드 2개 삽입
+    sum_of_min_two = heappop(card_list) + heappop(card_list)  # 가장 작은 두 값을 pop하여 합하고
+    [heappush(card_list, sum_of_min_two) for _ in range(2)]  # 그 합을 2번 push함
 
-print(sum(card_list))
+print(sum(card_list))  # 합하여 결과 출력
