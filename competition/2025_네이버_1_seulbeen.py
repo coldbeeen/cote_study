aeb37da664e2b1bdd9d257d04df5dba8241ef5fd
seulbeen <powerjsv12@gmail.com>
2025-04-05 15:56:43 +0900

    250405 2025 네이버 1번

---
+++ competition/2025_네이버_1_seulbeen.py
@@ -0,0 +1,33 @@
+# 모든 0인 경우인 수를 세면서, 발사/안발사를 각자 세면 되지 않을까?
+# 0인 인덱스를 다 찾냐 마냐 ...
+
+def solution(cylinder,a,n):
+    result=[]
+    mo=0
+    ja=0
+    no_bullet=list(filter(lambda x: cylinder[x]==0,range(len(cylinder))))
+    for idx in no_bullet:
+        cnt=0
+        flag=0
+        while cnt<a:
+            idx=(idx+1)%n
+            cnt+=1
+            if idx==1:
+                flag=1
+                break
+        if flag:
+            mo+=1
+        else:
+            mo+=1
+            ja+=1
+    for i in range(min(mo,ja),1,-1):
+        if mo%i==0 and ja%i==0:
+            mo//=i
+            ja//=i
+            break
+    result.append(ja)
+    result.append(mo)
+
+
+
+    return result
